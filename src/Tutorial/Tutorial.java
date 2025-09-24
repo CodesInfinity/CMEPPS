4877ec29e8170e6ee742fc07f461ccd6fc12f711
CodesInfinity <agusmatic21@gmail.com>
2025-09-24 18:52:22 +0200

    Cambios en la rama.

--- src/Tutorial/Tutorial.java
+++ src/Tutorial/Tutorial.java
@@ -5,6 +5,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!");
 		System.out.println("Estoy en mi primera rama!");
+		System.out.println("Cambio en la rama antes del merge!");
 	}
 
 }
