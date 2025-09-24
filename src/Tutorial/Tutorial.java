76a9dc526168e4a3bba185e1560f6a8a031fb5b2
CodesInfinity <agusmatic21@gmail.com>
2025-09-24 19:00:02 +0200

    Creo mi segunda rama!

--- src/Tutorial/Tutorial.java
+++ src/Tutorial/Tutorial.java
@@ -4,8 +4,7 @@ public class Tutorial {
 
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!");
-		System.out.println("Estoy en mi primera rama!");
-		System.out.println("Cambio en la rama antes del merge!");
+		System.out.println("Estoy en mi segunda rama!");
 	}
 
 }
